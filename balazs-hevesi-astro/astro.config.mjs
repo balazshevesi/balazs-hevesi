5bb107dff48cf7bf9335c38364b9125492e91247
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-01-18 22:37:08 +0100

    Add base path

--- balazs-hevesi-astro/astro.config.mjs
+++ balazs-hevesi-astro/astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), react()],
+  base: "balazs-hevesi/",
 });
