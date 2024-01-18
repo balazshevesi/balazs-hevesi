6a085b71c31856cc7e70177a9da0ab2e6b29089a
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-01-18 19:07:47 +0100

    Intergrate markdowns as projects

--- adorable-axis/tailwind.config.js
+++ adorable-axis/tailwind.config.js
@@ -4,8 +4,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    // ...
-  ],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
