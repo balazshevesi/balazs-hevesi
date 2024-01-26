75b6fffbe873a3dac78e260bfa80b0c7b30ba9bb
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-01-26 22:12:29 +0100

    Format

--- balazs-hevesi-astro/.prettierrc.mjs
+++ balazs-hevesi-astro/.prettierrc.mjs
@@ -1,7 +1,9 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  pluginSearchDirs: false,
+
   overrides: [
     {
       files: "*.astro",
